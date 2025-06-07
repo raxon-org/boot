25984307321b139930a0e0f818ea7d57fc78ab35
remco van der Velde <remco.pc@outlook.com>
2025-06-07 03:13:25 +0200

    init

--- src/Package/Trait/Init.php
+++ src/Package/Trait/Init.php
@@ -27,6 +27,11 @@ trait Init {
         Core::interactive();
         $object = $this->object();
         $options = App::options($object);
+        if(property_exists($options, 'lock') && $options->lock === 'release'){
+            $dir = new Dir();
+            $read = $dir->read($object->config('project.dir.data') . 'Lock' . $object->config('ds'));
+            ddd($read);
+        }
         $url_package = $object->config('project.dir.vendor') . 'raxon/boot/Data/Package.json';
         $class = File::basename($url_package, $object->config('extension.json'));
         $packages = $object->data_read($url_package);
